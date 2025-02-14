27f2b59cb64bb1fcdb4cce92c0dc875e50d39ed9
Ted Matuszewski <ted.matuszewski@nexteer.com>
2025-02-14 15:02:24 -0500

    feat: update Races component to highlight the next upcoming race and adjust layout

--- client/src/components/Races.vue
+++ client/src/components/Races.vue
@@ -8,17 +8,18 @@
         :slide-ratio="1 / 4"
         :dragging-distance="200"
         :initSlide="5"
-        fixed-height="130px"
+        :arrows="false"
+        fixed-height="200px"
         :breakpoints="{ 800: { visibleSlides: 2, slideMultiple: 2 } }">
             <vueper-slide v-for="(race, i) in races" :key="i">
                 <template #content>
                     <div class="card" style="width: 18rem;">
                         <div class="card-body">
-                            <h5 class="card-title">{{ race.name }}</h5>
+                            <h5 class="card-title">{{ race.name }} <span v-if="isNextUpcomingRace(race)" class="badge rounded-pill text-bg-warning">Next Race</span></h5>
                             <h6 class="card-subtitle mb-2 text-body-secondary">{{ formatDate(race.date) }}</h6>
                             <p class="card-text">{{ race.class }}</p>
-                            <!-- <a href="#" class="card-link">Card link</a>
-                            <a href="#" class="card-link">Another link</a> -->
+                            <a href="#" class="card-link">Results</a>
+                            <!-- <a href="#" class="card-link">Another link</a> -->
                         </div>
                     </div>
                 </template>
@@ -38,8 +39,22 @@
         const options = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' };
         return new Date(dateString).toLocaleDateString(undefined, options);
     }
+
+    function isNextUpcomingRace(race) {
+        const now = new Date();
+        const raceDate = new Date(race.date);
+
+        if (raceDate <= now) return false;
+
+        const upcomingRace = races.filter(r => new Date(r.date) > now)[0];
+        
+        return race.key === upcomingRace.key;
+    }
 </script>
 
 <style>
-
+.text-bg-warning {
+    color: #000 !important;
+    background-color: RGBA(255, 193, 7, 1) !important;
+}
 </style>

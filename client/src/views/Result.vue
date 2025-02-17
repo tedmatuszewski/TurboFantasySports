e8be6185ba32fe05de5a904b9f34ef5dc0ba5b8e
Ted Matuszewski <ted.matuszewski@nexteer.com>
2025-02-16 20:06:47 -0500

    feat: add race results table to Result view

--- client/src/views/Result.vue
+++ client/src/views/Result.vue
@@ -4,14 +4,33 @@
     <p>{{ route.params.race }}</p>
     <router-link :to="{ name: 'league', params: { id: route.params.id } }" class="btn btn-primary">League Home</router-link>
           
+    <table class="table table-sm table-hover">
+      <thead>
+        <tr>
+          <th>Position</th>
+          <th>Rider</th>
+          <th>Class</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="race in raceResults" :value="race.race">
+          <td>{{race.position}}</td>
+          <td>{{race.number}}</td>
+          <td>{{ race.class }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script setup>
   import { useRoute } from 'vue-router';
   import { ref,onMounted,computed, reactive  } from "vue";
+  import results from '../data/results.json';
   
   const route = useRoute();
+
+  let raceResults = results.filter(r => r.race === route.params.race);
 
   onMounted(async () => {
 
